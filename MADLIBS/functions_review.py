77ca1cdb064354071dd74544af2f1179658f0c25
AlphonseBrandon <alphonse.brand@gmail.com>
2022-05-08 22:39:41 +0100

    matlib project completed

---
+++ MADLIBS/functions_review.py
@@ -0,0 +1,26 @@
+'''
+String concatanation
+'''
+my_name = "Alphonse"
+your_name = "Rayhan"
+
+print("my name is {}".format(my_name))
+
+print(f"your name is {your_name}")
+
+'''
+Review of functions
+'''
+ # 1. in build functions
+ 
+print(len(your_name))
+his_name = input("What is his name: ")
+
+print(f"His name is: {his_name}")
+
+
+ # 2. Custom or user defined functions
+
+def user():
+     print("This is a custom function")
+user()
