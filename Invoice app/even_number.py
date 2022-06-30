299d4b1df194f67ae1c135681cf1c9f0156d3a2c
Alphonse Brandon <alphonse.brand@gmail.com>
2022-06-30 15:29:23 +0100

    Sorting even numbers from a list of numbers

---
+++ Invoice app/even_number.py
@@ -0,0 +1,10 @@
+# function that sorts even numbers from a set of numbers
+
+numbers = [1, 2, 3, 4, 5, 6]
+
+even_numbers = []
+
+for number in numbers:
+    if number % 2 == 0:
+        even_numbers.append(number)
+print(even_numbers)
